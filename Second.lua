def4454e10d4a7af1cd8ad79570b5bafae52949f
Sergey-Filippov <investfx09@gmail.com>
2024-05-07 16:31:05 +0300

    MACD-all, sec-code

--- Second.lua
+++ Second.lua
@@ -3,7 +3,7 @@
 function OnInit()  -- Первичная инициализация данных
     Name_Bot = "Бот на скользящих средних"          -- Наименование робота
     Version = " 1.0"                                -- Номер версии бота
-    Sec_code = ""                                   -- Наименование торгового тнструмента
+    Sec_code = "CNYRUBF"                                   -- Наименование торгового тнструмента
     Class_code =  "SPBFUT"
     Client_code = "567250R7WF1"                     -- Код клиента
     Firmid = "MC0061900000"  
@@ -14,11 +14,11 @@ function OnInit()  -- Первичная инициализация данных
     Stop = "off"                                    -- Режим работы робота пауза/работа
     Depo = getMoney(Client_code, Firmid, Tag, Cur_code).money_limit_available  -- Депозит
     Interval_M60 = 60                                 -- 60  Минут
-    Interval_M15 = 15                                  -- 5 Мин
-    INTERVAL_H4 = 240                              -- Дневной интервал
-    Period_Slow = 26                                -- Период сглаживания медленной EMA
-    Period_Fast = 12                                 -- Период сглаживания быстрой EMA
-    SDiapazon_H4 = 0                 
+    Interval_M15 = 15                                 -- 5 Мин
+    Interval_D1 = 1440                                -- Дневной интервал
+    Period_Slow = 26                                  -- Период сглаживания медленной EMA
+    Period_Fast = 12                                  -- Период сглаживания быстрой EMA
+    SDiapazon = 0                 
     Risk = 2
     Open_poz = 0                                        -- открытые позиции 
     log =  getScriptPath().."\\".."Save_one.txt"        -- Файл записи логов работы
@@ -44,6 +44,7 @@ function Log(str)                               -- Запись логов работы
        lg:flush() 
        lg:close()      
 end
+
 --------------------------------------------------------------------------------------
 function Sortirovka_selection()
 
@@ -112,88 +113,66 @@ sec_list = getClassSecurities(Class_code)
     return Tabl_sort
 end
 
-
-function EMA_60()  
-     
-    Ema = {}
-    DS_M60, Error = CreateDataSourse(Class_code, Sec_code, Interval_M60)
-    while (Error == nil or Error == "" and DS_M60:Size() == 0) do  sleep(10) end
-    if Error ~= nil or Error == nil then 
-        message("Ошибка подключения к графику: "..Error) 
-    end
-    DS_60:SetEmtyCallback()  
-    local Highprice_M60 = DS_M60:H(i)
-    local Lowprice_M60 = DS_M60:L(i) 
-    local Candles_M60 = DS_M60:Size()
-    local A_Slow = 2/(Period_Slow+1)
-    for i = (Candles_M60 - Ema_Slow), Candles_M60 do
-        Highprice_M60 = DS_M60:H(i)
-        Lowprice_M60 = DS_M60:L(i) 
-        Close_M60 = DS_M60:C(i)   
-        SDiapazon_M60 = SDiapazon_M60 + math.abs(tonumber(Highprice_M60)-tonumber(Lowprice_M60) )
-        if i >= (Candles_M60 - Period_Slow) then
-             
-                Ema[i] = Close_60 * A_Slow + Ema[i-1]*(1 - A_Slow)        -- Расчет эспотенциальной скользящей средней
-              
-        end
-        
-    end
- 
-end
 -----------------------------------------------------------------------------------
-function MACD_()
+function MACD_(Interval)
     Ema_Fast = {}
     Ema_Slow = {}
     MACD = {}
-    DS_H4, Error = CreateDataSource(Class_code, Sec_code, INTERVAL_H4)          -- Получение таблицы данных по D1
-    while (DS_H4:Size() == nil or DS_H4:Size() == 0) do                 -- Проверка получены ли данные
+    DS, Error = CreateDataSource(Class_code, Sec_code, Interval)          -- Получение таблицы данных по D1
+    while (DS:Size() == nil or DS:Size() == 0) do                         -- Проверка получены ли данные
        if Error ~= nil or Error ~="" then 
-        message("Ошибка подключения к графику: "..Error) 
-        break end
-     
+        message("Ошибка подключения к графику: "..Error)      
+    end
           sleep(100)
         end
     
     message(" Данные получены")
-    DS_H4:SetEmptyCallback()                                                     -- Подписка на автоматическое получение обновленных данных (Для D1 актуально?)
-    local Delita_price_H4 = 0 
-    local Highprice_H4 = 0
-    local Lowprice_H4 = 0
-    local Data_H4 = {}
-    local Candles_H4 = DS_H4:Size()                 -- Кол-во свечей по которым получены данные
+    DS:SetEmptyCallback()                                                     -- Подписка на автоматическое получение обновленных данных (Для D1 актуально?)
+    local Delita_Price = 0 
+    local High_price = 0
+    local Low_price = 0
+    local Data = {}
+    local Candless = DS:Size()                 -- Кол-во свечей по которым получены данные
     
-    Ema_Slow[1] = DS_H4:C(1) 
-    Ema_Fast[1] = DS_H4:C(1)
+    Ema_Slow[1] = DS:C(1) 
+    Ema_Fast[1] = DS:C(1)
     local A_Fast = 2/(Period_Fast+1)
     local A_Slow = 2/(Period_Slow+1)
-    for i = 2, Candles_H4 do
-        Highprice_H4 = DS_H4:H(i)                   -- High
-        Lowprice_H4 = DS_H4:L(i)                    -- Low
-        Data_H4 = DS_H4:T(i)
-            Ema_Slow[i] = DS_H4:C(i)
-            Ema_Fast[i] = DS_H4:C(i)
+    for i = 2, Candless do
+        High_price = DS:H(i)                   -- High
+        Low_price = DS:L(i)                    -- Low
+        Data = DS:T(i)
+            Ema_Slow[i] = DS:C(i)
+            Ema_Fast[i] = DS:C(i)
           
-            Ema_Slow[i] =  DS_H4:C(i)*A_Slow + Ema_Slow[i-1]*(1 - A_Slow)    -- Расчет медленной EMA
+            Ema_Slow[i] =  DS:C(i)*A_Slow + Ema_Slow[i-1]*(1 - A_Slow)    -- Расчет медленной EMA
            
-            Ema_Fast[i] =  DS_H4:C(i)*A_Fast + Ema_Fast[i-1]*(1 - A_Fast)    --Расчет быстрой EMA
+            Ema_Fast[i] =  DS:C(i)*A_Fast + Ema_Fast[i-1]*(1 - A_Fast)    --Расчет быстрой EMA
            
-            Delita_price_H4 = Delita_price_H4 + math.abs(Highprice_H4 -Lowprice_H4)    -- Расчет суммы всех дневных диапазонов
+            Delita_Price = Delita_Price + math.abs(High_price -Low_price)    -- Расчет суммы всех дневных диапазонов
        
-        if i >= Candles_H4 - Period_Slow then                                               --  Оставляем в памяти последние 22 значения
+        if i >= Candless - Period_Slow then                                               --  Оставляем в памяти последние 22 значения
             MACD[i-Period_Slow] = {}
             MACD[i-Period_Slow].macd = tonumber(string.format("%.6f", (Ema_Fast[i] - Ema_Slow[i])))       -- Расчет MACD
             MACD[i-Period_Slow].slow = tonumber(string.format("%.6f", Ema_Slow[i] ))
             MACD[i-Period_Slow].fast = tonumber(string.format("%.6f", Ema_Fast[i] ))
-            MACD[i-Period_Slow].data = tostring(Data_H4.day..' число '..Data_H4.month.." мес "..Data_H4.hour.." час "..Data_H4.min.." мин")
-            Str = tostring(MACD[i-Period_Slow].data.." -дата и время".."\n"..
-            MACD[i-Period_Slow].macd.." - MACD  "..MACD[i-Period_Slow].fast.." - fast  "..MACD[i-Period_Slow].slow.." - slow")
-           Log(Str)
+            MACD[i-Period_Slow].data =  Data
+           
         end  
-        SDiapazon_H4 = Delita_price_H4/Candles_H4                        -- Расчет среднедневного диапазона
+        SDiapazon = Delita_Price/Candless                        -- Расчет среднедневного диапазона
+        return MACD, SDiapazon
     end
    
 end
+
 ------------------------------------------------------------------------------------------------------------
+function OnStop()                           -- Функция остановки бота по нажатию кнопки "Stop"
+    Log("Остановка скрипта - Stop")
+    is_run = false
+    
+    return 1000   
+end
+--------------------------------------------------------------------------------------------------------------
 
 function main()
     Sortirovka_selection()
@@ -204,4 +183,12 @@ function main()
          "\n".." Возможно купить или продать контрактов ".. Tabl_sort[i].can_buy_sell ) 
          sleep(200)
     end
+    MACD_(Interval_D1)
+    message(tostring(#MACD))
+    for i = 1, #MACD do
+        Str = tostring(MACD[i].data.day.." -дата и время".."\n"..
+        MACD[i].macd.." - MACD  "..MACD[i].fast.." - fast  "..MACD[i].slow.." - slow")
+       Log(Str)
+    end
+    Log(SDiapazon)
 end
